0bd7262679a48f3707a49b1986e4d177659005ee
Dmitry Sky <dmitry.sky@icloud.com>
2015-04-09 08:59:22 -0600

    damn README file

--- allergies.rb
+++ allergies.rb
@@ -1,33 +1,3 @@
-# require 'pry'
-
-# class Allergy
-
-#     HASHY = {
-#       1 => "eggs",
-#       2 => "peanuts",
-#       4 => "shellfish",
-#       8 => "strawberries",
-#       16 => "tomatoes",
-#       32 => "chocolate",
-#       64 => "pollen",
-#       128 => "cats"
-#     }
-  
-#   def allergy_score(*allergen)
-#     allergen.reduce(0) { |sum,word| sum += HASHY.invert[word] }
-#   end
-
-#   def allergen(score)
-#     check_these = HASHY.values.combination(2).detect { |a,b| a + b == score }
-#     spit_out_words = check_these.map { |num| HASHY.invert[num] }
-#   end
-
-# end
-
-# a = Allergy.new
-# a.allergy_score('peanuts','chocalate')
-# a.allergen(34)
-
 class Allergy
 
   def self.allergy_lookup
