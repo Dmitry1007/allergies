259e3ea2b095d44af89b8b69fcc35a2c7735352d
Dmitry Sky <dmitry.sky@icloud.com>
2015-04-09 08:57:51 -0600

    swaped keys values and set up diagnose method

--- allergies.rb
+++ allergies.rb
@@ -1,35 +1,62 @@
-require 'pry'
+# require 'pry'
 
-class Allergy
+# class Allergy
 
-  def initialize
-    @allergy_hashy = {
-      '' => 0,
-      'eggs' => 1,
-      'peanuts' => 2,
-      'shellfish' => 4,
-      'strawberries' => 8,
-      'tomatoes' => 16,
-      'chocalate' => 32,
-      'pollen' => 64,
-      'cats' => 128,
-    }
-  end
+#     HASHY = {
+#       1 => "eggs",
+#       2 => "peanuts",
+#       4 => "shellfish",
+#       8 => "strawberries",
+#       16 => "tomatoes",
+#       32 => "chocolate",
+#       64 => "pollen",
+#       128 => "cats"
+#     }
   
-  def allergy_score(*allergen)
-    allergen.reduce(0) { |sum,word| sum += @allergy_hashy[word] }
-  end
+#   def allergy_score(*allergen)
+#     allergen.reduce(0) { |sum,word| sum += HASHY.invert[word] }
+#   end
 
-  def allergen(score)
-    check_these = @allergy_hashy.values.combination(2).detect { |a,b| a + b == score }
-    spit_out_words = check_these.map { |num| @allergy_hashy.invert[num] }
-  end
+#   def allergen(score)
+#     check_these = HASHY.values.combination(2).detect { |a,b| a + b == score }
+#     spit_out_words = check_these.map { |num| HASHY.invert[num] }
+#   end
 
-end
+# end
 
 # a = Allergy.new
 # a.allergy_score('peanuts','chocalate')
 # a.allergen(34)
+
+class Allergy
+
+  def self.allergy_lookup
+    {
+      1 => "eggs",
+      2 => "peanuts",
+      4 => "shellfish",
+      8 => "strawberries",
+      16 => "tomatoes",
+      32 => "chocolate",
+      64 => "pollen",
+      128 => "cats"
+    }
+  end
+
+  def self.diagnose(score)
+    allergy_report = []
+    keys = allergy_lookup.keys
+    keys.reverse.each do |key|
+      if key <= score
+        allergy_report << allergy_lookup[key]
+        score -= key
+      end
+    end
+    allergy_report
+  end
+end
+
+
 
 
 
--- allergies_test.rb
+++ allergies_test.rb
@@ -4,46 +4,13 @@ require_relative 'allergies'
 
 class AllergyTest < Minitest::Test
 
-  def test_it_returns_1_when_eggs_are_passed_in
-    assert_equal 1, Allergy.new.allergy_score('eggs')
-  end
-
-  def test_that_it_can_take_multiple_allergies_and_reduce
-    assert_equal 3, Allergy.new.allergy_score('eggs', 'peanuts')
-    assert_equal 34, Allergy.new.allergy_score('peanuts','chocalate')
-  end
-
   def test_it_returns_2_foods_for_which_person_has_allergies
-    assert_equal ['peanuts','chocalate'], Allergy.new.allergen(34)
-    assert_equal ['eggs','peanuts'], Allergy.new.allergen(3)
-    assert_equal ['shellfish','strawberries'], Allergy.new.allergen(12)
+    assert_equal ['chocolate','peanuts'], Allergy.diagnose(34)
+    assert_equal ['peanuts','eggs'], Allergy.diagnose(3)
+    assert_equal ['strawberries','shellfish'], Allergy.diagnose(12)
   end
 
   def test_it_returns_3_foods_for_which_person_has_allergies
-    assert_equal ['eggs','peanuts','chocalate'], Allergy.new.allergen(35)
+    assert_equal ['chocolate','peanuts','eggs'], Allergy.diagnose(35)
   end
-
 end
-
-
-# An allergy test produces a single numeric score which contains 
-# the information about all the allergies the person has 
-# (that they were tested for).
-
-# The list of items (and their value) that were tested are:
-
-# eggs (1)
-# peanuts (2)
-# shellfish (4)
-# strawberries (8)
-# tomatoes (16)
-# chocolate (32)
-# pollen (64)
-# cats (128)
-# So if Tom is allergic to peanuts and chocolate, he gets a score of 34.
-
-# Now, given just that score of 34, your program should be able to say:
-
-# Whether Tom is allergic to any one of those allergens listed above.
-# All the allergens Tom is allergic to.
-
